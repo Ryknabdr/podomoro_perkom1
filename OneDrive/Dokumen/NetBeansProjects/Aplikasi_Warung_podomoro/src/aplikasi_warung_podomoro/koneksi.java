67beb5652fc5b9a457b0acd74669a0002ebf46c3
abdurraihan <abdurraihan@REYKHAN.mshome.net>
2024-10-22 17:21:45 +0700

    Revert "Revert """
    This reverts commit b66ed87d6b84654b2748c7fdd5c952d943db5142.

--- OneDrive/Dokumen/NetBeansProjects/Aplikasi_Warung_podomoro/src/aplikasi_warung_podomoro/koneksi.java
+++ OneDrive/Dokumen/NetBeansProjects/Aplikasi_Warung_podomoro/src/aplikasi_warung_podomoro/koneksi.java
@@ -12,7 +12,7 @@ import javax.swing.JOptionPane;
  * @author abdurraihan
  */
 public class koneksi {
-  private String url = "jdbc:mysql://localhost/db_warung_podomoro";
+   private String url = "jdbc:mysql://localhost/db_warung_podomoro";
    private String username_xampp = "root";
    private String password_xampp = "";
    private Connection con;
